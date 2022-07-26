4ed26c44e3d840afb12edacb844271671ecae05b
Caedis <Caedis@users.noreply.github.com>
2022-07-26 03:16:33 -0500

    Only function in survival

--- src/main/java/com/cleanroommc/blockdelayremover/mixins/MixinPlayerControllerMP.java
+++ src/main/java/com/cleanroommc/blockdelayremover/mixins/MixinPlayerControllerMP.java
@@ -4,6 +4,7 @@ package com.cleanroommc.blockdelayremover.mixins;
 import net.minecraft.client.multiplayer.PlayerControllerMP;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,9 +16,13 @@ public abstract class MixinPlayerControllerMP
 {
     @Shadow
     private int blockHitDelay;
+    @Shadow
+    private GameType currentGameType;
 
     @Inject(method = "onPlayerDamageBlock", at=@At("HEAD"))
     private void onPlayerDamageBlock(BlockPos posBlock, EnumFacing directionFacing, CallbackInfoReturnable<Boolean> cir){
-        blockHitDelay = 0;
+        if (currentGameType.isSurvivalOrAdventure()){
+            blockHitDelay = 0;
+        }
     }
 }
